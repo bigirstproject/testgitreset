2d3bcd01768f507d8115472fde4cc5f94ea94ec9
lidexian <lidexian@ppmoney.com>
2019-10-22 19:06:16 +0800

    第十二次提交

--- app/src/main/java/com/sunsun/testgitrest/BActivity.java
+++ app/src/main/java/com/sunsun/testgitrest/BActivity.java
@@ -1,15 +1,18 @@
 package com.sunsun.testgitrest;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BActivity extends AppCompatActivity {
+    private static final String TAG  ="BActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ss");
     }
     
 }
