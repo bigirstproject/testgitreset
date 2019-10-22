d9131e0336750ee186606c8293028fecf6c10cf4
lidexian <lidexian@ppmoney.com>
2019-10-22 18:31:00 +0800

    第七次提交

--- app/src/main/java/com/sunsun/testgitrest/BActivity.java
+++ app/src/main/java/com/sunsun/testgitrest/BActivity.java
@@ -1,18 +1,15 @@
 package com.sunsun.testgitrest;
 
-
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AActivity extends AppCompatActivity {
+public class BActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-
-
+    
 }
